fad5e664e46a66fc2f19cdd813b65f1bea1e20a8
Sonya <sonya.castro.gomez@gmail.com>
2021-04-05 10:07:53 -0500

    Menu Bar Style

---
+++ index.js
@@ -0,0 +1,14 @@
+let menuBarButton = document.getElementById("menu-bar-button");
+let menuBarLinks = document.getElementById("menu-bar-links");
+
+function openCloseMenu() {
+    if (menuBarLinks.style.display != "none") {
+        menuBarLinks.style.display = "none";
+        menuBarButton.style.transform = "rotate(180deg)";
+
+    } else {
+        menuBarButton.style.transform = "rotate(360deg)";
+        menuBarLinks.style.display = "block";
+    }
+}
+// .menu-bar button:active
