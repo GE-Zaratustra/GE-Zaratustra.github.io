8ff541b68590e2c7cfa6762797876057215a3a45
Sonya <sonya.castro.gomez@gmail.com>
2021-05-07 08:36:31 -0500

    Add Imges

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let menuBarButton = document.getElementById("menu-bar-button");
 let menuBarLinks = document.getElementById("menu-bar-links");
 
+/**
+ * Abre y cierra el menu en dispositivos pequeños como celulares
+ */
 function openCloseMenu() {
     if (menuBarLinks.style.display != "none" && menuBarLinks.style.display != "") {
         menuBarLinks.style.display = "none";
@@ -10,4 +13,15 @@ function openCloseMenu() {
         menuBarButton.style.transform = "rotate(360deg)";
         menuBarLinks.style.display = "block";
     }
+}
+
+/**
+ * Dada una URL, crea un request y regresa true si esta URL existe. En caso contrario, regresa falso
+ */
+function UrlExists(url) {
+    let http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+
+    return http.status != 404;
 }
